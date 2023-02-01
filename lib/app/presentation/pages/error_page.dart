e625aadab151c9e1f78b7903b8066591dbab2af2
Zhurnev <dzhurnev@gmail.com>
2023-02-01 17:46:01 +0200

    Added en arb file and moved to a new version.

--- lib/app/presentation/pages/error_page.dart
+++ lib/app/presentation/pages/error_page.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:test_layout/l10n/l10n.dart';
 
 class ErrorPage extends StatelessWidget {
   const ErrorPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Oops, something went wrong.'),
+            Text(l10n.errorMessage),
             ElevatedButton(
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: const Text('Go back'),
+              child: Text(l10n.errorButton),
             ),
           ],
         ),
